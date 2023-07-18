cb1cde228d942bcf5dea73b20faa270712a291d9
gabriel.werpel-fernandes@danone.com <gabriel.werpel-fernandes@danone.com>
2023-07-18 21:19:46 +0000

    update file

--- filler_model.py
+++ filler_model.py
@@ -1,20 +1,8 @@
 # Databricks notebook source
-2+2
-
-# COMMAND ----------
-
-# MAGIC %py
-# MAGIC print('Hello World!')
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC # This is a markdown
 # MAGIC
 
 # COMMAND ----------
 
-print('Hello withou Magic')
-
-# COMMAND ----------
-
+print('Hello without Magic')
