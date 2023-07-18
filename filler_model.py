7d4bf37848822df921584d92100d9768f029e16e
Gabriel Werpel <74022089+GabrielWerpel@users.noreply.github.com>
2023-07-19 08:52:07 +1200

    new file

---
+++ filler_model.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+2+2
+
+# COMMAND ----------
+
+# MAGIC %py
+# MAGIC print('Hello World!')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # This is a markdown
+# MAGIC
+
+# COMMAND ----------
+
+print('Hello withou Magic')
+
+# COMMAND ----------
+
