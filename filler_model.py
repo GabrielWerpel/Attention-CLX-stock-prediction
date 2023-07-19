a23838e0e861468db6a71a6477a249312028ce83
Gabriel Werpel <74022089+GabrielWerpel@users.noreply.github.com>
2023-07-19 17:08:17 +1200

    updated filler_model.py

--- filler_model.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC # This is a markdown
-# MAGIC
-
-# COMMAND ----------
-
-print('Hello There!')
---
+++ src/filler_model.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Filler Model - Gemini Project
+# MAGIC
+
+# COMMAND ----------
+# MAGIC %md
+# MAGIC ## Import Libraries
+# MAGIC
+
+# COMMAND ----------
+import pandas as pd
+# COMMAND ----------
+# Load data
+raw_df = pd.read_csv(r"C:\Users\WERPELGA\Desktop\Databricks\01.IDE_Github_Databricks_Sync\filler_data_head_1_for_prediction_small.csv")
+# COMMAND ----------
+#Order data by datetime
+raw_df.sort_values(by=['datetime_nz'], inplace=True, ascending=True)
+# COMMAND ----------
+#Separate df into target_head_y and features_x
+y = raw_df['target_head_y']
+X = raw_df.drop('target_head_y', axis=1)
+# COMMAND ----------
+# MAGIC %md
+# MAGIC ## Remove useless features
+# MAGIC
+# COMMAND ----------
+#Drop NetWeightHead01 and DateTime columns
+X.drop('MtFillerHeadData.NetWeightHead01', axis=1, inplace=True) # repeated column from target_head_y while extracting from historian
+X.drop('DateTime', axis=1, inplace=True) # repeated column from datetime_nz while extracting from historian
+# COMMAND ----------
